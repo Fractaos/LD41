7ac1ca5beb9f28b60179d3ebfb50d8bb04708659
Frank <fr1303@hotmail.fr>
2018-04-21 15:59:15 +0200

    Classe Weapon abstraite de base

--- LudumDare41/LudumDare41/ShooterPhase/Weapon.cs
+++ LudumDare41/LudumDare41/ShooterPhase/Weapon.cs
@@ -4,11 +4,40 @@ using Microsoft.Xna.Framework.Graphics;
 
 namespace LudumDare41.ShooterPhase
 {
-    public class Weapon : Sprite
+    public abstract class Weapon : Sprite
     {
-        public Weapon(Texture2D texture, Vector2 position) : base(texture, position)
+        private float _fireSpeed, _bulletSpeed;
+        private int _numberBulletInLoader, _totalBullet;
+
+        protected Weapon(Texture2D texture, Vector2 position) : base(texture, position)
         {
 
         }
+
+        public float FireSpeed
+        {
+            get => _fireSpeed;
+            set => _fireSpeed = value;
+        }
+
+        public float BulletSpeed
+        {
+            get => _bulletSpeed;
+        }
+
+        public int NumberBulletInLoader
+        {
+            get => _numberBulletInLoader;
+        }
+
+        public int TotalBullet
+        {
+            get => _totalBullet;
+            set => _totalBullet = value;
+        }
+
+        public abstract void Update(GameTime time);
+
+        public new abstract void Draw(SpriteBatch spriteBatch);
     }
 }
