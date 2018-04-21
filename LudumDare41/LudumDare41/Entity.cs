55f9a52c7862ab80e56f04c7019e0252d6da402b
Frank <fr1303@hotmail.fr>
2018-04-21 09:04:20 +0200

    First commit

---
+++ LudumDare41/LudumDare41/Entity.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LudumDare41
+{
+    public class Entity
+    {
+        public Vector2 Position;
+
+        public Entity(Vector2 position)
+        {
+            Position = position;
+        }
+    }
+}
---
+++ LudumDare41/LudumDare41/HUD/HUD.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LudumDare41
+{
+    public class Hud
+    {
+        public Hud()
+        {
+
+        }
+
+        public void Update()
+        {
+
+        }
+
+        public void Draw(SpriteBatch batch)
+        {
+
+        }
+    }
+}
