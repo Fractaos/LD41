7cc1e8036ae202951b675d6eb0624c257e1dc11a
Unknown <dupycournau@gmail.com>
2018-04-21 09:27:18 +0200

    Nettoyage assets 2

--- LudumDare41/LudumDare41/Graphics/Assets.cs
+++ LudumDare41/LudumDare41/Graphics/Assets.cs
@@ -10,8 +10,6 @@ namespace LudumDare41.Graphics
         public static Texture2D PixelW, PixelB;
 
         //SPRITE
-        public static Texture2D Heart, Bouton, Skull, ButtonSet, ButtonSet2;
-        public static Dictionary<string, Texture2D> ButtonDic, ButtonDic2;
         //SON
         //FONT
         public static SpriteFont Font;
@@ -23,10 +21,6 @@ namespace LudumDare41.Graphics
             PixelW = Utils.CreateTexture(1, 1, Color.White);
             PixelB = Utils.CreateTexture(1, 1, Color.Black);
 
-            #region DEMOGUI
-            LoadGuiDemo();
-            #endregion
-
             //SPRITE
 
             //SON
@@ -34,42 +28,9 @@ namespace LudumDare41.Graphics
             //FONT
             Font = Main.Content.Load<SpriteFont>("Assets/Fonts/littlefont");
 
-            LoadGuiDemo();
         }
 
-        private static void LoadGuiDemo()
-        {
-            Heart = Main.Content.Load<Texture2D>("DEMOS/DemoGUI/heart");
-            Skull = Main.Content.Load<Texture2D>("DEMOS/DemoGUI/skull");
-            ButtonSet = Main.Content.Load<Texture2D>("DEMOS/DemoGUI/buttonset");
-            ButtonSet2 = Main.Content.Load<Texture2D>("DEMOS/DemoGUI/buttonset2");
-
-            ButtonDic = new Dictionary<string, Texture2D>
-            {
-                { "topleft", Utils.Slice(new Rectangle(0, 0, 32, 32), ButtonSet) },
-                { "top", Utils.Slice(new Rectangle(32, 0, 32, 32), ButtonSet) },
-                { "topright", Utils.Slice(new Rectangle(64, 0, 32, 32), ButtonSet) },
-                { "left", Utils.Slice(new Rectangle(0, 32, 32, 32), ButtonSet) },
-                { "middle", Utils.Slice(new Rectangle(32, 32, 32, 32), ButtonSet) },
-                { "right", Utils.Slice(new Rectangle(64, 32, 32, 32), ButtonSet) },
-                { "bottomleft", Utils.Slice(new Rectangle(0, 64, 32, 32), ButtonSet) },
-                { "bottom", Utils.Slice(new Rectangle(32, 64, 32, 32), ButtonSet) },
-                { "bottomright", Utils.Slice(new Rectangle(64, 64, 32, 32), ButtonSet) }
-            };
-
-            ButtonDic2 = new Dictionary<string, Texture2D>
-            {
-                { "topleft", Utils.Slice(new Rectangle(0, 0, 32, 32), ButtonSet2) },
-                { "top", Utils.Slice(new Rectangle(32, 0, 32, 32), ButtonSet2) },
-                { "topright", Utils.Slice(new Rectangle(64, 0, 32, 32), ButtonSet2) },
-                { "left", Utils.Slice(new Rectangle(0, 32, 32, 32), ButtonSet2) },
-                { "middle", Utils.Slice(new Rectangle(32, 32, 32, 32), ButtonSet2) },
-                { "right", Utils.Slice(new Rectangle(64, 32, 32, 32), ButtonSet2) },
-                { "bottomleft", Utils.Slice(new Rectangle(0, 64, 32, 32), ButtonSet2) },
-                { "bottom", Utils.Slice(new Rectangle(32, 64, 32, 32), ButtonSet2) },
-                { "bottomright", Utils.Slice(new Rectangle(64, 64, 32, 32), ButtonSet2) }
-            };
-        }
+    
         #endregion
     }
 }
