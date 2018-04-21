504408ae3cd531c21c3437b4ecab70af708e7c51
Frank <fr1303@hotmail.fr>
2018-04-21 10:45:41 +0200

    Create ShooterScreen

---
+++ LudumDare41/LudumDare41/Screens/ShooterScreen.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace LudumDare41.Screens
+{
+    public class ShooterScreen : Screen
+    {
+        public override void Create()
+        {
+
+        }
+
+        public override void Update(GameTime time)
+        {
+
+        }
+
+        public override void Draw()
+        {
+
+        }
+    }
+}
