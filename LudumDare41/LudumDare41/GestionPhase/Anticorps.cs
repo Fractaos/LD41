b8f6b105bf45c094fd779e5766ead3690059cc7a
Unknown <dupycournau@gmail.com>
2018-04-21 16:43:27 +0200

    Lol

--- LudumDare41/LudumDare41/GestionPhase/Anticorps.cs
+++ LudumDare41/LudumDare41/GestionPhase/Anticorps.cs
@@ -5,19 +5,23 @@ using System.Text;
 using System.Threading.Tasks;
 
 using LudumDare41.Graphics;
+using Microsoft.Xna.Framework;
 
 namespace LudumDare41.GestionPhase
 {
     public enum Organ { None, Head, Arms, Corps, Legs}
 
-    public class Anticorps
+    public class Anticorps : Sprite
     {
         public int level;
         public Bodypart ActualPart;
 
-        public Anticorps()
+        public bool End;
+
+        public Anticorps(Vector2 position, Bodypart part = null) : base(Utils.CreateTexture(40, 40, Color.Red), position)
         {
-            ActualPart = null;
+            End = false;
+            ActualPart = part;
         }
 
         public void Update(float time)
